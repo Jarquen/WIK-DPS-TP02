e43e26acede450b13fd3389345a7e7b6940ec711
Jarquen <quentin.jarlot@gmail.com>
2022-10-17 15:50:35 +0200

    First commit

--- build/index.js
+++ build/index.js
@@ -6,7 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.get('/ping', (req, res) => {
-    return res.status(200).json(req.headers);
+    try {
+        return res.status(200).json(req.headers);
+    }
+    catch (error) {
+        return res.status(500).json({ message: "undefined error" });
+    }
 });
 app.get('*', (req, res) => {
     return res.status(404).json({ message: "NOT FOUND" });
